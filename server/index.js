cf838115d331227e55568fd5d266462a9e8cc659
Ondřej Moravec <moravec@devground.cz>
2023-12-14 12:38:47 +0100

    Initial project setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
